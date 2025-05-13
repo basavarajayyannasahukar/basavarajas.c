2b896a2b7e83a0a82da6ca305e4f9dabe822ee1c
basavarajayyannasahukar <basavarajayyannasahukar@gmail.com>
2025-05-13 16:28:51 +0530

    Create even_or_odd.c

---
+++ even_or_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter the value for a");
+    scanf("%d",&a);
+    printf("enter the value of b");
+    scanf("%d",&b);
+    sum=a+b;
+    diference=a-b;
+    product=a*b;
+    quatiant=a/b;
+    printf("%d
+    
+}
