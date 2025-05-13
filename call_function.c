4211ea4007b2d168c37a6e6c4968ecb8cded1b38
basavarajayyannasahukar <basavarajayyannasahukar@gmail.com>
2025-05-13 15:24:52 +0530

    Create call_function.c

---
+++ call_function.c
@@ -0,0 +1,10 @@
+// function deffination //function call
+#include <stdio.h>
+int a,b;
+int main() {
+    //function deffination
+    printf("basavaraj");
+    //function call
+    printf("c=a+b");
+    return 0;
+}
