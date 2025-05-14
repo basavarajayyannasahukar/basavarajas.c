6292859a05aa7ba3e6aa8fd267bda89fd58cec51
basavarajayyannasahukar <basavarajayyannasahukar@gmail.com>
2025-05-14 16:16:49 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 int main(){
-    int n,i,c=0;
-    printf("enter the array element:");
-    scanf("%d",&n);
-    if (n=0){
-    printf("1");
-}
-else{
-    for(i=1;i<n;i++){
-        c*=i;
+    int n=5, result=1,i;
+    if(n==0){
+        printf("1");
     }
-    printf("%d",c);
-    
-     }
+    else{
+        for(i=1;i<=5;i++){
+            result = result * i;
+            printf("%d\n",result);
+        }
+        printf("%d",result);
+    }
 }
